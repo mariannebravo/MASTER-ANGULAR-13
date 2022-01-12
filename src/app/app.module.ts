d7cab90d8fa0b253106f0d57599e51a4289d4232
Marianne Bravo <mloubravo@outlook.com>
2022-01-12 14:41:42 -0300

    doing forms with validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppRouteModule } from './app-route.module';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Header/header.component';
-import { FormsModule } from '@angular/forms';
 import { RecipesComponent } from './Recipes/recipes.component';
 import { RecipeListComponent } from './Recipes/recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './Recipes/recipe-detail/recipe-detail.component';
@@ -13,7 +15,6 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
-import { AppRouteModule } from './app-route.module';
 import { RecipeEditComponent } from './Recipes/recipe-edit/recipe-edit.component';
 
 @NgModule({
@@ -30,7 +31,7 @@ import { RecipeEditComponent } from './Recipes/recipe-edit/recipe-edit.component
     RecipeStart,
     RecipeEditComponent,
   ],
-  imports: [BrowserModule, FormsModule, AppRouteModule],
+  imports: [BrowserModule, FormsModule, AppRouteModule, HttpClientModule],
   providers: [ShoppingListService],
   bootstrap: [AppComponent],
 })
